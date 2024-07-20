54d2508c725f0341a24eb46172e08c9a7ced167b
axelvalles <agabrielv1799@gmail.com>
2024-07-20 14:38:56 -0500

    first commit

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,153 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
+import { backbroundMap, floorMap } from "../mapped/map-1";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  keys: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+  player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  floor: Phaser.Physics.Arcade.StaticGroup;
 
-    constructor ()
-    {
-        super('Game');
+  constructor() {
+    super("Game");
+  }
+
+  preload() {
+    this.load.spritesheet(
+      "characters",
+      "assets/Tilemap/tilemap-characters.png",
+      {
+        frameWidth: 24,
+        frameHeight: 24,
+        spacing: 1,
+      }
+    );
+
+    this.load.spritesheet("tilemap", "assets/Tilemap/tilemap.png", {
+      frameWidth: 18,
+      frameHeight: 18,
+      spacing: 1,
+    });
+
+    this.load.spritesheet(
+      "background",
+      "assets/Tilemap/tilemap-backgrounds.png",
+      {
+        frameWidth: 24,
+        frameHeight: 24,
+        spacing: 1,
+      }
+    );
+  }
+
+  create() {
+    //config
+    this.keys = this.input.keyboard?.createCursorKeys();
+
+    // enviroment
+
+    this.floor = this.physics.add.staticGroup();
+
+    const numColumns = 30;
+    const numRows = 20;
+
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numColumns; col++) {
+        // Calcular el índice del tile en el array
+        const index = row * numColumns + col;
+        const tileIndex = backbroundMap[index] - 1;
+
+        // Calcular las coordenadas x e y para cada tile
+        const x = col * 24;
+        const y = row * 24;
+
+        // Agregar el sprite del tile en la posición calculada
+        this.add.sprite(x, y, "background", tileIndex);
+      }
     }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numColumns; col++) {
+        // Calcular el índice del tile en el array
+        const index = row * numColumns + col;
+        const tileIndex = floorMap[index] - 1;
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        if (tileIndex > -1) {
+          // Calcular las coordenadas x e y para cada tile
+          const x = col * 18;
+          const y = row * 18;
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+          this.floor.create(x, y, "tilemap", tileIndex);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
+          //   this.add.sprite(x, y, "tilemap", tileIndex);
+        }
+      }
     }
+
+    this.floor.refresh();
+
+    // entities
+    this.player = this.physics.add
+      .sprite(20, 100, "characters")
+      .setOrigin(0, 0)
+      .setCollideWorldBounds(true);
+    this.player.flipX = true;
+
+    // colliders
+    this.physics.add.collider(this.player, this.floor);
+
+    // animation
+    this.anims.create({
+      key: "player-walk",
+      frames: this.anims.generateFrameNumbers("characters", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player-idle",
+      frames: this.anims.generateFrameNumbers("characters", {
+        start: 0,
+        end: 0,
+      }),
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player-jump",
+      frames: this.anims.generateFrameNumbers("characters", {
+        start: 1,
+        end: 1,
+      }),
+      repeat: -1,
+    });
+  }
+
+  update(time: number, delta: number): void {
+    if (this.keys?.left.isDown) {
+      this.player.x -= 2;
+      this.player.flipX = false;
+
+      if (this.player.body.touching.down) {
+        this.player.anims.play("player-walk", true);
+      }
+    } else if (this.keys?.right.isDown) {
+      this.player.x += 2;
+      this.player.flipX = true;
+
+      if (this.player.body.touching.down) {
+        this.player.anims.play("player-walk", true);
+      }
+    } else {
+      if (this.player.body.touching.down) {
+        this.player.anims.play("player-idle", true);
+      }
+    }
+
+    if (this.keys?.up.isDown && this.player.body.touching.down) {
+      this.player.anims.play("player-jump", true);
+      this.player.setVelocityY(-250);
+    }
+  }
 }
